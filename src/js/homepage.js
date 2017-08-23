6b0319ce50d983ceed2b6c236ac83eb38446c366
Uniquec <837046740@qq.com>
2017-08-23 17:03:11 +0800

    数据填充 评论框点击事件

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -1,4 +1,4 @@
-import {visitors,smalltalks} from './config';
+import {visitors, smalltalks} from './config';
 $(function () {
     let current = 0;
 
@@ -26,11 +26,128 @@ $(function () {
 
     $.get(visitors, (res)=> {
         setText(res, current);
-        console.log(res);
+        // console.log(res);
     });
 
     $.get(smalltalks, (res)=> {
-        console.log(res);
+        createSmalltalk(res);
     })
 
+    function createSmalltalk(smalltalkarr) {
+        // console.log(smalltalkarr);
+        for (let i = 0; i < smalltalkarr.length; i++) {
+            let data = smalltalkarr[i];
+            // console.log(data);
+            let smalltalk = $(` <div class="small-talk">`);
+            let talkerMessage = $(`
+                <div class="talker-message">
+                    <img src="../image/head-portrait.jpg" class="talker-image">
+                    <div class="talker-tips">
+                        <div class="talker-name">岁月静好。</div>
+                        <div>${data.time}</div>
+                    </div>
+                    <span class="glyphicon glyphicon-menu-down" aria-hidden="true"></span>
+                </div>
+            `);
+            let talkContent = $(`
+                <div class="talk-content">
+                    <div class="words">${data.content}</div>
+                    <div class="talk-image"><img src="${data.pic}"></div>
+                    <div class="mobile-logo"><i class="iconfont icon-p-shouji"></i>来自 ${data.equipment}</div>
+                </div>
+            `);
+            let likesarr = data.star.join('、');
+            let a = data.starNumber === data.star.length ? '共' : '等';
+            let talkMessage = $(`
+                 <div class="talk-message">
+                    <div class="looks-people">浏览${data.watchNumber}次<i class="iconfont icon-dianzan1"></i><i
+                            class="iconfont icon-comment"></i><i class="iconfont icon-share"></i></div>
+                    <div class="grey-line"></div>
+                    <div class="likes-people">
+                        <div class="icon-bgc"><i class="iconfont icon-dianzan1"></i></div>
+                        <div class="likes-name">${likesarr}${a}${data.starNumber}人觉得很赞</div>
+                    </div>
+                </div>
+            `);
+
+            let specificComments = $(`<div class="specific-comments"></div>`);
+            for (let j = 0; j < data.comment.length; j++) {
+                let commentdata = data.comment[j];
+                // console.log('commentdata');
+                // console.log(commentdata);
+                let ReSpecificComments = $(`
+                    <div class="comment">
+                        <div class="reviewer-comment">
+                            <img src="${commentdata.pic}">
+                            <div class="comment-content">
+                                <div><span>${commentdata.from}</span>  : ${commentdata.content}</div>
+                                <div class="comment-time">${commentdata.time}</div>
+                            </div>
+                        </div>
+                    </div>
+                `);
+                let replyComments = $(' <div class="reply-comments"></div>')
+                if(commentdata.reply) {
+                    for (let k = 0; k < commentdata.reply.length; k++) {
+                        let rcommentdata = commentdata.reply[k];
+                        // console.log('rcommentdata');
+                        // console.log(rcommentdata);
+                        let replyComment = $(`
+                            <div class="reply-comments-image">
+                                <img src="${rcommentdata.pic}">
+                            </div>
+                            <div class="reply-comments-content">
+                                <div><span>${rcommentdata.from} </span>回复<span>${rcommentdata.to} </span>: ${rcommentdata.content}</div>
+                                <div class="comment-time">${rcommentdata.time}</div>
+                            </div>
+                        </div>
+                    `);
+                        replyComments.append(replyComment);
+                    }
+                }
+                specificComments.append(ReSpecificComments).append(replyComments);
+
+            }
+            let talkComment = $(`
+                <div class="talk-comment">
+                    <textarea placeholder="评论" class="comment-text"></textarea>
+                    <i class="iconfont icon-camera"></i>
+                </div>
+            `);
+
+
+
+
+
+            smalltalk.append(talkerMessage).append(talkContent).append(talkMessage).append(specificComments).append(talkComment);
+
+            $('.homepage-rmessage').append(smalltalk);
+
+        }
+
+        let publishComment = $(`
+             <div class="publish-comment">
+                <div class="comment-expression">
+                    <i class="iconfont icon-biaoqing"></i>
+                    <div class="expression-scircle"></div>
+                </div>
+                <i class="iconfont icon-aite"></i>
+                <div class="white-circle"></div>
+                <div class="private-comment">私密评论</div>
+                <i class="iconfont icon-diamonds"></i>
+                <button>发表</button>
+             </div>
+        `);
+
+        $(".comment-text").click(function (e) {
+            $(".comment-text").css("height","40px");
+            $(".icon-camera").css("padding-top","8px");
+            let commentText = $(e.currentTarget);
+            commentText.css("height","77px");
+            commentText.attr('placeholder','');
+            $(commentText.next()).css("padding-top","45px");
+            $(commentText.next()).css("color","#e6b85c");
+            commentText.parent().parent().append(publishComment);
+        });
+    }
 });
